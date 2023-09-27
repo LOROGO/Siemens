6a537911520fcff1f825eeaecf2bfef6450bbc0b
Juraj Banas <juraj.banas3@gmail.com>
2023-09-27 09:01:12 +0200

    GetFolderPath - reading path from user

--- Siemens/Program.cs
+++ Siemens/Program.cs
@@ -1,10 +1,58 @@
-﻿namespace Siemens
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace Siemens
 {
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                string path = GetFolderPath();
+
+                if (path == null)
+                {
+                    break;
+                }
+
+
+
+
+
+
+
+
+
+            }
+
         }
+
+        static string GetFolderPath()
+        {
+            Console.WriteLine("Please provide a folder or a JSON with folder information:");
+            string path = Console.ReadLine();
+            if (Directory.Exists(path))
+            {
+
+                return path;
+            }
+            else
+                return null;
+
+        }
+
+
+
+
+
+
+
+
+
+
+
     }
 }
